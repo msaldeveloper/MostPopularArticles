8b0f6f63a15990d5048ed1d2ba822c272523699e
msalda <msalda53@outlook.com>
2024-10-09 11:59:29 -0600

    creating xCode project

---
+++ mostPopularArticles/mostPopularArticles/mostPopularArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mostPopularArticlesApp.swift
+//  mostPopularArticles
+//
+//  Created by Mario Alberto Saldaña Martínez on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct mostPopularArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
